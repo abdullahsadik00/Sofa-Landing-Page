75f398a88cf1a8444ddb7ba832454c63b86425bd
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-11-29 16:52:27 +0530

    Project Initialization

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      lightGray: "#F6F6F6",
+      lightYellow: "#F3A30B",
+      lightPink: "#F07171",
+    },
+  },
+  plugins: [],
+};
